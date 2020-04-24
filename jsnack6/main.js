38d4976a725555bc0ade95907ade89d96ca111e8
Marco De Clara <marcodeclara@gmail.com>
2020-04-24 10:14:03 +0200

    Small adjustments

--- jsnack6/main.js
+++ jsnack6/main.js
@@ -1,23 +1,20 @@
 // Stampa le potenze di 2 fino a 1000
 
-// initializing index
-var i = 0;
+// initializing power, base case 2 ^ 0
+var power = 1;
 
-//initializing power
-var power = 0;
+// initializing index, first index is 1
+var i = 1;
 
 // upper limit value
 var max = 1000;
 
 // while power is less than max number
 while (power < max) {
+    // print said power
+    console.log(power);
     // power of 2 ^ index
     power = Math.pow(2, i);
-    // if power is less than max number
-    if (power < max) {
-        // print said power
-        console.log(power);
-    }
     // increase index
     i++;
 }
